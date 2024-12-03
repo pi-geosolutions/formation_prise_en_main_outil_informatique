43ed58f30ec92907c0748e8add339e6f669ff647
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2024-12-03 06:41:53 +0100

    Improve PDF generation. Add pdf documents

--- make_pdfs.sh
+++ make_pdfs.sh
@@ -2,6 +2,8 @@
 
 pandoc -V geometry:margin=35mm -o programme.pdf programme.md
 pandoc -V geometry:margin=35mm -o notes.pdf notes.md
- pandoc -V geometry:margin=35mm -o clavier.pdf clavier/readme.md clavier/raccourcis.md
- pandoc -V geometry:margin=35mm -o windows.pdf windows/readme.md
- pandoc -V geometry:margin=35mm -o linux.pdf linux/readme.md
+pandoc -V geometry:margin=35mm -o formationPriseEnMain.pdf readme.md \
+    clavier/readme.md \
+    windows/readme.md \
+    linux/readme.md \
+    BD/readme.md
