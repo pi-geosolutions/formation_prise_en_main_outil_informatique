c8eca86824173a2bae6722971f122e92b2434710
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2025-10-20 14:10:26 +0200

    Commit last updates from 2024 session

---
+++ windows/files/blah.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+def blablah(many:int):
+    output = ""
+    for i in range(many):
+        output += "blah"
+    print(output)
+
+def deprecated():
+    print("This is a useless and deprecated function")
+    print("You can comment me")
