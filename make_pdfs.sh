465faf671868782cc6d42ecb8157e8d081da5114
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2025-10-20 17:58:56 +0200

    Add pagebreaks in PDF
    More lisible if each chapter starts on a fresh page

--- make_pdfs.sh
+++ make_pdfs.sh
@@ -2,8 +2,8 @@
 
 pandoc -V geometry:margin=35mm -o programme.pdf programme.md
 pandoc -V geometry:margin=35mm -o notes.pdf notes.md
-pandoc -V geometry:margin=35mm -o formationPriseEnMain.pdf readme.md \
-    clavier.md \
-    windows.md \
-    linux.md \
+pandoc -V geometry:margin=35mm -o formationPriseEnMain.pdf readme.md .latex/pagebreak.md  \
+    clavier.md .latex/pagebreak.md  \
+    windows.md .latex/pagebreak.md  \
+    linux.md .latex/pagebreak.md  \
     bd.md
